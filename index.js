7850100c71a1097894ed3448d2d9bf0d36b640c0
alissonraposo <alissonraposo@hotmail.com>
2025-06-26 12:59:35 -0300

    ...

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const fs = require('fs');
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = '192.168.1.204';
+// const hostname = '127.0.0.1';
 const port = 3000;
 
 const html = fs.readFileSync('index.html', 'utf8');
 
 const server = http.createServer((req,res) => {
-    console.log("req",req);
+    // console.log("req",req);
     console.log("req.url",req.url);
     
-    if(req.url == "/index.html"){
+    if(req.url == "/index1.html"){
         res.end(html);        
     }else{
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Olá Mundo');
+        res.end(fs.readFileSync('.' + req.url));
+        // res.statusCode = 200;
+        // res.setHeader('Content-Type', 'text/plain');
+        // res.end('Olá Mundo');
     }
 });
 
